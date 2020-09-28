434a3afde93f6a92f0f2e97d33d58f78a7fd5e24
Anders <anderst.bendtsen@gmail.com>
2020-09-28 11:20:38 +0200

    added Userinterface.cs

---
+++ DatingApp/UserInterface.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Figgle;
+
+namespace DatingApp
+{
+    public class UserInterface
+    {
+        public static void BuildFrontpage()
+        {
+            Console.Clear();
+            Console.WriteLine(FiggleFonts.Standard.Render("\t DatingApp!"));
+
+            Console.SetCursorPosition(2, 10);
+            Console.Write("ny bruger? j/n: ");
+        }
+
+        public static void BuildCreateNewUser()
+        {
+            Console.Clear();
+            Console.WriteLine(FiggleFonts.Standard.Render("\t DatingApp!"));
+
+            Console.SetCursorPosition(2, 10);
+            Console.Write("Desired username: ");
+            Console.SetCursorPosition(2, 11);
+            Console.Write("Firstname       : ");
+            Console.SetCursorPosition(2, 12);
+            Console.Write("Lastname        : ");
+            Console.SetCursorPosition(2, 13);
+            Console.Write("Email           : ");
+        }
+
+        public static void BuildLoginPage()
+        {
+            Console.Clear();
+            Console.WriteLine(FiggleFonts.Standard.Render("\t DatingApp!"));
+
+            Console.SetCursorPosition(2, 10);
+            Console.Write("Username     :");
+            Console.SetCursorPosition(2, 11);
+            Console.Write("Password     :");
+
+        }
+
+        public static void BuildCreateProfile()
+        {
+            Console.Clear();
+
+            Console.SetCursorPosition(2, 10);
+            Console.WriteLine("Please fill in information about yourself");
+            Console.SetCursorPosition(2, 11);
+            Console.WriteLine("This information will be displayed to other users");
+            Console.SetCursorPosition(2, 12);
+            Console.WriteLine("Fields marked with a * are required");
+
+            Console.SetCursorPosition(2, 14);
+            Console.WriteLine("* Gender (m/f)             :");
+            Console.SetCursorPosition(2, 15);
+            Console.WriteLine("* Interrested in (m/f/b)   :");
+
+        }
+    }
+}
