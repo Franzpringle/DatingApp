0cfc659e33bdb03a0ee9dc3d9b34e67256fbc7ff
Anders <anderst.bendtsen@gmail.com>
2020-09-29 00:53:23 +0200

    tilføjet mere menu funktionalitet

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -8,13 +8,18 @@ namespace DatingApp
     {
         static void Main(string[] args)
         {
-            string username, pass = string.Empty;
+            string pass = string.Empty;
+            ConsoleKeyInfo menuChoice;
 
-            UserInterface.BuildFrontpage();
+            do
+            {
+                UserInterface.BuildFrontpage();
+                Console.SetCursorPosition(62, 10);
+                menuChoice = Console.ReadKey();
 
-            username = Console.ReadLine();
+            } while (menuChoice.Key != ConsoleKey.NumPad1 && menuChoice.Key != ConsoleKey.D1 && menuChoice.Key != ConsoleKey.NumPad2 && menuChoice.Key != ConsoleKey.D2);
 
-            if(username.ToLower() == "j")
+            if (menuChoice.Key == ConsoleKey.NumPad2 || menuChoice.Key == ConsoleKey.D2)
             {
 
                 UserInterface.BuildCreateNewUser();
@@ -51,8 +56,40 @@ namespace DatingApp
 
                 Console.SetCursorPosition(2, 17);
                 Console.WriteLine($"Test af output: {u.Firstname} {u.Lastname} {u.Username} {u.Email} {u.Password}");
+
+            }
+            if (menuChoice.Key == ConsoleKey.NumPad1 || menuChoice.Key == ConsoleKey.D1)
+            {
+                UserInterface.BuildLoginPage();
+                User loginUser = new User();
+
+                Console.SetCursorPosition(17, 10);
+                loginUser.Username = Console.ReadLine();
+                Console.SetCursorPosition(17, 11);
+                ConsoleKey key;
+                do
+                {
+                    var keyInfo = Console.ReadKey(intercept: true);
+                    key = keyInfo.Key;
+
+                    if (key == ConsoleKey.Backspace && pass.Length > 0)
+                    {
+                        Console.Write("\b \b");
+                        pass = pass[0..^1];
+                    }
+                    else if (!char.IsControl(keyInfo.KeyChar))
+                    {
+                        Console.Write("*");
+                        pass += keyInfo.KeyChar;
+                    }
+                } while (key != ConsoleKey.Enter);
+
+                loginUser.Password = pass;
+
+                Console.SetCursorPosition(2, 13);
+                Console.WriteLine($"Testing output: {loginUser.Username} {loginUser.Password}");
+
                 Console.ReadKey();
-                
             }
 
 
